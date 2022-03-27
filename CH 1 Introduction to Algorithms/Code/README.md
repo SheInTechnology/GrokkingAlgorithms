c6aaf895ae73816fa03cb0cab4ba229fd0a39b8c
Ameenah <ameenah.k.a.m@gmail.com>
2022-03-27 15:03:42 +0300

    chapter5-code addition

--- CH 1 Introduction to Algorithms/Code/README.md
+++ CH 1 Introduction to Algorithms/Code/README.md
@@ -1,1 +1,3 @@
 # Code description
+
+This folder includes several code exaples collected from the internet about hastables written in Java and Javascript.
---
+++ CH 5 Hash Tables/Code/FindPair.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+ 
+public class FindPair
+{
+    // Function to find a pair in an array with a given sum using hashing
+    public static void findPair(int[] nums, int target)
+    {
+        // create an empty HashMap
+        Map<Integer, Integer> map = new HashMap<>();
+ 
+        // do for each element
+        for (int i = 0; i < nums.length; i++)
+        {
+            // check if pair (nums[i], target-nums[i]) exists
+ 
+            // if the difference is seen before, print the pair
+            if (map.containsKey(target - nums[i]))
+            {
+                System.out.printf("Pair found (%d, %d)",
+                    nums[map.get(target - nums[i])], nums[i]);
+                return;
+            }
+ 
+            // store index of the current element in the map
+            map.put(nums[i], i);
+        }
+ 
+        // we reach here if the pair is not found
+        System.out.println("Pair not found");
+    }
+ 
+    public static void main (String[] args)
+    {
+        int[] nums = { 8, 7, 2, 5, 3, 1 };
+        int target = 10;
+ 
+        findPair(nums, target);
+    }
+}
